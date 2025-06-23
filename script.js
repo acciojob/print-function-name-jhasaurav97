c664f2f79172fb54d3b88308ffde8268b478846a
Saurav <jhasaurav593@gmail.com>
2025-06-23 22:32:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function functionName(){
+	return arguments.callee.name;
+}
+alert(functionName());
+
+function AccioJob(){
+	return arguments.callee.name;
+}
+alert(AccioJob());
